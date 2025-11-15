4997344a7e2713ed4d0ae0d06e681e396df7d264
srinivas-191 <srinivasrajoli2002@gmail.com>
2025-11-15 15:40:48 +0530

    aihero

--- frontend/src/pages/Recommendations.jsx
+++ frontend/src/pages/Recommendations.jsx
@@ -1,16 +1,16 @@
-import React from "react";
-import AiHero from "../components/layout/ui/aiRecom/aiHero";
-import AIDashboard from "../components/layout/ui/aiRecom/AIDashboard";
-import AiResults from "../components/layout/ui/aiRecom/AiResults";
+// import React from "react";
+// import AiHero from "../components/layout/ui/aiRecom/aiHero";
+// import AIDashboard from "../components/layout/ui/aiRecom/AIDashboard";
+// import AiResults from "../components/layout/ui/aiRecom/AiResults";
 
-const Recommendations = () => {
-  return (
-    <div>
-      <AiHero />
-      <AIDashboard />
-      <AiResults />
-    </div>
-  );
-};
+// const Recommendations = () => {
+//   return (
+//     <div>
+//       <AiHero />
+//       <AIDashboard />
+//       <AiResults />
+//     </div>
+//   );
+// };
 
-export default Recommendations;
+// export default Recommendations;
